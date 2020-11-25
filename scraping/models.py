488efbf493f3247c9cd2bd29477692f02d2d6dd6
Sergey K <serjkasmail@gmail.com>
2020-11-25 11:52:13 +0300

    create new model lang

--- scraping/models.py
+++ scraping/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(verbose_name='Название города', max_length=50)
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(verbose_name='Название города',
+                            max_length=50,
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Название города'
         verbose_name_plural = 'Название городов'
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(verbose_name='Название языка',
+                            max_length=50,
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Название ЯП'
+        verbose_name_plural = 'Названия языков'
 
     def __str__(self):
         return self.name
