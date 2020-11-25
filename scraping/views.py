56db621c0b0901f8430d6e5b5bf09b04c72403d4
Sergey K <serjkasmail@gmail.com>
2020-11-26 00:13:33 +0300

    add filter by add from django form

--- scraping/views.py
+++ scraping/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
+
+from .forms import FindForm
 from .models import Vacancy
 
 
 def home_view(request):
-    # print(request.GET)
+    print(request.GET)
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
+
     qs = []
     if city or language:
         _filter = {}
         if city:
-            _filter['city__name'] = city
+            _filter['city__id'] = city
         if language:
-            _filter['language__name'] = language
+            _filter['language__id'] = language
 
         qs = Vacancy.objects.filter(**_filter)
 
     content = {
-        'object_list': qs
+        'object_list': qs,
+        'form': form
     }
     return render(request, 'scraping/home.html', content)
