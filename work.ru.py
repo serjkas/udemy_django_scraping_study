17af087e8ee25b3c7d8ecf28278944d1434ffc25
Sergey K <serjkasmail@gmail.com>
2020-11-29 15:00:25 +0300

    start ru work - hh

--- work.ru.py
+++ work.ru.py
@@ -5,37 +5,43 @@ from bs4 import BeautifulSoup as BS
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
 
-domain_ua = 'https://www.work.ua'
+
+domain_ru = 'https://hh.ru'
 
 
-url_ua = 'https://www.work.ua/jobs-python/'
+url_ru = 'https://hh.ru/search/vacancy?area=&fromSearchLine=true&st=searchVacancy&text=python'
 
-res = requests.get(url_ua, headers=headers)
+res = requests.get(url_ru, headers=headers)
 jobs_list = []
 errors = []
 
 
 if res.status_code == 200:
     soup = BS(res.content, 'html.parser')
-    main_div = soup.find('div', id='pjax-job-list')
+    # main_div = soup.find('div', id='vacancy-serp')
+    #print(soup)
+    main_div = soup.find('div', id='HH-React-Root')
+    #print(main_div)
     if main_div:
-        div_list = main_div.find_all('div', attrs={'class': 'job-link'})
+        div_list = main_div.find_all('div', attrs={'class': 'vacancy-serp-item'})
         a = 1
         for div in div_list:
-            title = div.find('h2')
+            title = div.find('a')
             href = title.a['href']
             descriptions = div.p.text
             company = 'no name'
             logo = div.find('img')
             if logo:
                 company = logo['alt']
-                jobs_list.append({'title': title.text, 'url': domain_ua + href,
+                jobs_list.append({'title': title.text, 'url': domain_ru + href,
                                   'description': descriptions, 'company': company})
     else:
-        errors.append({'url': url_ua, 'code': res.status_code, 'title': 'div has not founded'})
-else:
-    errors.append({'url': url_ua, 'code': res.status_code, 'title': 'Page not found'})
+        errors.append({'url': url_ru, 'code': res.status_code, 'title': 'div has not founded'})
 
+else:
+    errors.append({'url': url_ru, 'code': res.status_code, 'title': 'Page not found'})
+
+print(errors)
 h = codecs.open('work.txt', 'w', 'utf-8')
 h.write(str(jobs_list))
 h.close()
