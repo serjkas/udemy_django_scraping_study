5cf2e7350787e66cf6b6bc77702b0090231d1e40
Sergey K <serjkasmail@gmail.com>
2020-12-06 16:51:07 +0300

    add info message

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib import messages
 from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
+
+User = get_user_model()
 
 
 def login_view(request):
@@ -29,6 +31,7 @@ def register_view(request):
         new_user = form.save(commit=False)
         new_user.set_password(form.cleaned_data['password'])
         new_user.save()
+        messages.success(request, 'Пользователь добавлен на сайт')
         return render(request, 'accounts/register_done.html',
                       {'new_user': new_user})
     return render(request, 'accounts/register.html', {'form': form})
@@ -39,10 +42,28 @@ def update_view(request):
         user = request.user
         if request.method == 'POST':
             form = UserUpdateForm(request.POST)
-        else:
-            form = UserUpdateForm(
-                initial={'city': user.city, 'language': user.language,
-                         'send_email': user.send_email})
-            return render(request, 'accounts/update.html', {'form': form})
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['send_email']
+                user.save()
+                messages.success(request, 'Пользователь изменен.')
+                return redirect('accounts:update')
+
+        form = UserUpdateForm(
+            initial={'city': user.city, 'language': user.language,
+                     'send_email': user.send_email})
+        return render(request, 'accounts/update.html', {'form': form})
     else:
         return redirect('accounts:login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            qs = User.objects.get(pk=user.pk)
+            qs.delete()
+            messages.error(request, 'Пользователь удален.')
+    return redirect('home')
